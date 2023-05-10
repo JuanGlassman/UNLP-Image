0f461907e516372097ee2fb4305c25cc9512d3bd
JuanGlassman <128055960+JuanGlassman@users.noreply.github.com>
2023-05-10 20:44:06 -0300

    Add files via upload

--- Menu_Principal.py
+++ Menu_Principal.py
@@ -3,6 +3,11 @@ import os
 import io
 from PIL import Image
 import editar_perfil as perfil_editar
+import Configuracion as configuracion
+import Generar_Meme as generar_meme
+import Generar_Collage as collage
+
+
 #import configuracion as configuracion1 #importo el archivo configuracion
 sg.theme ('LightGrey4')
 
@@ -26,34 +31,38 @@ def me (perfil):
     ruta_fotos1 = os.path.join(os.getcwd(),"Fotos","signo.png")
     ruta_fotos2 = os.path.join(os.getcwd(),"Fotos","config.png")
     boton_ayuda=[[sg.Button(key ='configuracion', button_color=('LightGrey', 'grey'),image_data=abrir_foto(ruta_fotos2),border_width=0,image_size=(100,100), image_subsample=2),
-                 sg.Button(key ='Ayuda', button_color=('LightGrey', 'grey'), image_data=abrir_foto(ruta_fotos1),border_width=0,image_size=(100,100),image_subsample=2)]]
+                 sg.Button(key ='ayuda', button_color=('LightGrey', 'grey'), image_data=abrir_foto(ruta_fotos1),border_width=0,image_size=(100,100),image_subsample=2)]]
     boton_imagen=[[sg.Button(image_data=abrir_foto(perfil['Foto']), image_size=(100,100),image_subsample=2,key='editar_perfil')],
         [sg.Text((f"-{perfil['Alias']}-"),justification='center', size=(10, 1), font=('Helvetica', 12), border_width=2, text_color='black')]]
 
-    barra_principal = [[sg.Column(boton_imagen, element_justification='left', expand_x=True),sg.Column(boton_ayuda, element_justification='rigth', expand_x=True)]
-                       #[sg.Column(boton_ayuda, element_justification='rigth', expand_x=True)]
-                    ]
+    barra_principal = [[sg.Column(boton_imagen, element_justification='left', expand_x=True),
+                        sg.Column(boton_ayuda, element_justification='rigth', expand_x=True)]]
 
-    layout = [
-        [barra_principal],
-        #[sg.Column(boton_ayuda, element_justification='rigth', expand_x=True)],
-        #[sg.Column(boton_imagen, element_justification='left', expand_x=True)],
-        [sg.Button("Etiquetar Imagenes", size=(40, 4), button_color=('Black', 'mediumpurple'), font=('Helvetica', 17), border_width=2)], #mediumorchid
-        [sg.Button("Generar Meme", size=(40, 4), button_color=('Black', 'LightBlue'), font=('Helvetica', 17), border_width=2)],
-        [sg.Button("Generar Collage", size=(40, 4), button_color=('Black', 'skyblue'), font=('Helvetica', 17), border_width=2)],
+    layout = [[barra_principal],
+        [sg.Button("Etiquetar Imagenes", size=(40, 4), button_color=('Black', 'mediumpurple'), font=('Helvetica', 17), border_width=2, key='etiquetar')], #mediumorchid
+        [sg.Button("Generar Meme", size=(40, 4), button_color=('Black', 'LightBlue'), font=('Helvetica', 17), border_width=2,key='generar_meme')],
+        [sg.Button("Generar Collage", size=(40, 4), button_color=('Black', 'skyblue'), font=('Helvetica', 17), border_width=2,key='generar_collage')],
         [sg.Button("Salir", size=(40, 4), button_color=('Black', 'steelblue'), font=('Helvetica', 17), border_width=2)]
     ]
 
-    window = sg.Window('',layout, element_justification='c', size=(1366,768))
+    window = sg.Window('',layout, element_justification='c', size=(1366,768), resizable=True )
     while True:
         event, values = window.read()
-        if ((event == sg.WIN_CLOSED)or (event == "Salir")):
+        if ((event == sg.WIN_CLOSED) or (event == "Salir")):
             break
         if event == "configuracion": #solo puedo acceder una vez a configuracion, despues se rompe
             window.hide()
+            configuracion.conf(perfil["Alias"])
             window.UnHide()
+        if event == "ayuda":
+            foto_ayuda = os.path.join(os.getcwd(),"Fotos","ayuda.png")
+            sg.popup(image=foto_ayuda)
         if event == 'editar_perfil':
-            perfil_editar.editar_perfil(perfil)
-            window.refresh()
-                #sg.Popup(configuracion1.conf()) #con este se puede seguir accediendo a configuracion pero solo deja guardar la primera vez
+            perfil = perfil_editar.editar_perfil(perfil)
+            window['editar_perfil'].update(image_data=abrir_foto(perfil['Foto']), image_size=(100,100),image_subsample=2)
+        if event == 'generar_meme':
+            generar_meme.meme()
+        if event == 'generar_collage':
+            collage.coll()
     window.close()
+    return perfil
